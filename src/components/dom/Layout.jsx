963238c9cc54cb74ee70fd1a0faafc5abdee4cdd
Julio Merisio <contatomerisio@gmail.com>
2024-04-23 23:17:04 -0300

    check performance

--- src/components/dom/Layout.jsx
+++ src/components/dom/Layout.jsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useRef } from 'react'
-import dynamic from 'next/dynamic'
-const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
+// import dynamic from 'next/dynamic'
+// const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
 
 const Layout = ({ children }) => {
   const ref = useRef()
@@ -19,18 +19,18 @@ const Layout = ({ children }) => {
       }}
     >
       {children}
-      <Scene
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          pointerEvents: 'none',
-        }}
-        eventSource={ref}
-        eventPrefix='client'
-      />
+      {/*<Scene*/}
+      {/*  style={{*/}
+      {/*    position: 'fixed',*/}
+      {/*    top: 0,*/}
+      {/*    left: 0,*/}
+      {/*    width: '100vw',*/}
+      {/*    height: '100vh',*/}
+      {/*    pointerEvents: 'none',*/}
+      {/*  }}*/}
+      {/*  eventSource={ref}*/}
+      {/*  eventPrefix='client'*/}
+      {/*/>*/}
     </div>
   )
 }
